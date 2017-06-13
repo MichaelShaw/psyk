2bf1bf5ef4da53abdeae0cc0c06ac8608d4dd517
Michael Shaw <michaeljshaw@gmail.com>
2017-06-14 01:13:58 +1000

    Comment

---
+++ src/server/mod.rs
@@ -0,0 +1,8 @@
+
+
+
+// we need to rethink this in terms of:
+// 1. State Cells. (Client, Game)
+// 2. State Transitions to cells. (and how we determined if these are allowed ... rejecting of state transitions)
+// 3. Subscribers to state cells.
+// 4. Recordings of state transitions are replays.
